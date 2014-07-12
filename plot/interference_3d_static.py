840f83206071038c3fb27c31460f9616462f1fe0
Alex Gutikov <gutikoff@gmail.com>
2014-07-12 15:43:07 +0300

    Interference plot.

---
+++ plot/interference_3d_static.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+
+from mpl_toolkits.mplot3d import axes3d
+import matplotlib.pyplot as plt
+import numpy as np
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+
+X = np.linspace(-5*np.pi, 10*np.pi, 100)
+Y = np.linspace(-5*np.pi, 10*np.pi, 100)
+
+X, Y = np.meshgrid(X, Y)
+
+R1 = np.sqrt(X**2 + Y**2)
+R2 = np.sqrt((X-4*np.pi)**2 + Y**2)
+Z = np.sin(R1)/R1 + np.sin(R2)/R2
+
+
+ax.plot_wireframe(X, Y, Z, rstride=1, cstride=1)
+
+plt.show()
+
+
---
+++ plot/interference_projection_animation.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+
+"""
+A simple example of an animated plot
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+fig, ax = plt.subplots()
+
+x = np.arange(-3*np.pi, 3*np.pi, 0.01)        # x-array
+line, = ax.plot(x, np.sin(x))
+
+
+D = 3
+L = 3
+
+speed = 20.0
+
+# sin(x**2 + D**2)/sqrt(x**2 + D**2) + sin((x - L)**2 + D**2)/sqrt((x - L)**2 + D**2)
+
+def animate(i):
+#    line.set_ydata(np.sin(x+i/10.0))  # update the data
+	i /= speed
+
+#	line.set_ydata(2*(np.sin((x**2 + D**2)-i)/np.sqrt(x**2 + D**2) + np.sin(((x+L)**2 + D**2)-i)/np.sqrt((x+L)**2 + D**2))**2)
+#	line.set_ydata((np.sin((x**2 + D**2)-i)/np.sqrt(x**2 + D**2))**2)
+
+	line.set_ydata(np.sin((x**2 + D**2)-i)/np.sqrt(x**2 + D**2) + np.sin(((x+L)**2 + D**2)-i)/np.sqrt((x+L)**2 + D**2))
+#	line.set_ydata(np.sin((x**2 + D**2)-i)/np.sqrt(x**2 + D**2))
+	return line,
+
+#Init only required for blitting to give a clean slate.
+def init():
+    line.set_ydata(np.ma.array(x, mask=True))
+    return line,
+
+ani = animation.FuncAnimation(fig, animate, np.arange(1, 2000), init_func=init,
+    interval=25, blit=True)
+plt.show()
+
