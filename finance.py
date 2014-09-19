3bb31eb7f8d94576c6fe4a280cbf86c2b2b8735e
Aleksey Gutikov <ahutsikau@exadel.com>
2014-09-19 18:22:45 +0300

    finance in progress, sqlalchemy

--- finance.py
+++ finance.py
@@ -11,6 +11,8 @@ from sqlalchemy import func
 
 SQLITE_URL = ""
 
+################################# COMMAND-LINE PARAMETERS
+
 usage_text = "Usage: %prog [sqlite_database_file]\n" \
 		"\n" \
 		"If no specified database file - then sqlite :memory: database will be used.\n"
@@ -138,9 +140,9 @@ if current_schema_revision == 2:
 
 		Column('name', String, nullable=False, unique=True),
 
-		Column('currency_code_num_3', String(3), nullable=False, unique=True),
-		Column('currency_code_ascii_3', String(3), nullable=False, unique=True),
-		Column('currency_exponent', Integer)
+		Column('code_num_3', String(3), nullable=False, unique=True),
+		Column('code_ascii_3', String(3), nullable=False, unique=True),
+		Column('exponent', Integer)
 	)
 	currency.create()
 
@@ -155,61 +157,64 @@ if current_schema_revision == 2:
 		Column('id', Integer, primary_key=True),
 				   
 		Column('user_id', Integer, ForeignKey('users.id'), nullable=False),
+		Column('comments', String),
 		
 		Column('currency_id', Integer, ForeignKey('currency.id')),
 		Column('amount', Integer, nullable=False),
 		Column('period_unit_id', Integer, ForeignKey('period_units.id')),
 		Column('duration', Integer, nullable=False),
-		
-		Column('changed_timestamp', DateTime, default=datetime.now),
+
 		Column('start_date', DateTime),
 		Column('end_date', DateTime)
 	)
-
-	regular_fixed_payment_periods = Table('regular_fixed_payment_periods', meta,
-		Column('id', Integer, primary_key=True),
-
-		Column('title', String, nullable=False, unique=True),
-		Column('comments', String),
-
-		Column('currency_id', Integer, ForeignKey('currency.id')),
-		Column('amount', Integer, nullable=False),
-		Column('period_unit_id', Integer, ForeignKey('period_units.id')),
-		Column('duration', Integer, nullable=False),
-		
-		Column('start_date', DateTime),
-		Column('end_date', DateTime, nullable=True)
-	)
-	regular_fixed_payment_periods.create()
 
 	payments = Table('payments', meta,
 		Column('id', Integer, primary_key=True),
 
 		Column('title', String, nullable=False, unique=True),
-		Column('comments', String),
-
-		Column('regular_fixed_payment_period_id', Integer, ForeignKey('regular_fixed_payment_periods.id'), nullable=True),
-
-		Column('currency_id', Integer, ForeignKey('currency.id')),
-		Column('approx_amount', Integer, nullable=False)
+		Column('comments', String)
 	)
 	payments.create()
+
+	payment_periods = Table('payment_periods', meta,
+		Column('id', Integer, primary_key=True),
+
+		Column('payment_id', Integer, ForeignKey('payments.id'), nullable=False),
+
+		Column('currency_id', Integer, ForeignKey('currency.id')),
+		Column('amount', Integer, nullable=False),
+		Column('period_unit_id', Integer, ForeignKey('period_units.id')),
+		Column('duration', Integer, nullable=True),
+		
+		Column('start_date', DateTime),
+		Column('end_date', DateTime, nullable=True)
+	)
+	payment_periods.create()
+
+	exchange_event_data = Table('exchange_event_data', meta,
+		Column('id', Integer, primary_key=True),
+
+		Column('src_currency_id', Integer, ForeignKey('currency.id'),  nullable=True),
+		Column('dst_currency_id', Integer, ForeignKey('currency.id'),  nullable=True),
+		Column('course', Integer, nullable=True),
+		Column('course_exponent', Integer,  nullable=True),
+		Column('location', String, nullable=True)
+	)
 
 	events = Table('events', meta,
 		Column('id', Integer, primary_key=True),
 
 		Column('commit_timestamp', DateTime, default=datetime.now),
-		Column('timestamp', DateTime),
+		Column('event_timestamp', DateTime),
 
-		# 0 - complex event (sequence of events)
-		# 1 - income (user deposit)
-		# 2 - payment
-		# 3 - slice
-		# 4 - merge
-		# 5 - exchange
+		# 0 - complex event (sequence of events) - several events has it's id as parent_event_id
+		# 1 - income (user deposit) - src_user_id not Null
+		# 2 - payment - dst_payment_id not Null
+		# 3 - slice - utility event, parent_event_id not Null
+		# 4 - merge - utility event, parent_event_id not Null
+		# 5 - exchange - exchange_event_data_id not Null
 		Column('type', Integer, nullable=False),
 
-		Column('title', String),
 		Column('comments', String, nullable=True),
 
 		Column('parent_event_id', Integer, ForeignKey('events.id'), nullable=True),
@@ -217,11 +222,7 @@ if current_schema_revision == 2:
 		Column('src_user_id', Integer, ForeignKey('users.id'), nullable=True),
 		Column('dst_payment_id', Integer, ForeignKey('payments.id'), nullable=True),
 
-		Column('exchange_src_currency_id', Integer, ForeignKey('currency.id'),  nullable=True),
-		Column('exchange_dst_currency_id', Integer, ForeignKey('currency.id'),  nullable=True),
-		Column('exchange_course', Integer, nullable=True),
-		Column('exchange_course_exponent', Integer,  nullable=True),
-		Column('exchange_location', String, nullable=True)
+		Column('exchange_event_data_id', Integer, ForeignKey('exchange_event_data.id'), nullable=True)
 	)
 	events.create()
 
@@ -231,9 +232,7 @@ if current_schema_revision == 2:
 		Column('currency_id', Integer, ForeignKey('currency.id')),
 		Column('amount', Integer, nullable=False),
 
-		Column('alive', Boolean),
-
-		Column('src_event_id', Integer, ForeignKey('events.id'), nullable=True),
+		Column('src_event_id', Integer, ForeignKey('events.id'), nullable=False),
 		Column('dst_event_id', Integer, ForeignKey('events.id'), nullable=True)
 	)
 	money_chunks.create()
@@ -251,9 +250,10 @@ if current_schema_revision == 2:
 else:
 	currency = Table('currency', meta, autoload=True)
 	period_units = Table('period_units', meta, autoload=True)
-	regular_fixed_payment_periods = Table('regular_fixed_payment_periods', meta, autoload=True)
 	users_payment_obligations = Table('users_payment_obligations', meta, autoload=True)
+	payment_periods = Table('regular_fixed_payment_periods', meta, autoload=True)
 	payments = Table('payments', meta, autoload=True)
+	exchange_event_data = Table('exchange_event_data', meta, autoload=True)
 	events = Table('events', meta, autoload=True)
 	money_chunks = Table('money_chunks', meta, autoload=True)
 	__metadata_revisions = Table('__metadata_revisions', meta, autoload=True)
@@ -284,10 +284,10 @@ if current_metadata_revision == 1:
 		{'name': 'phone'})
 
 	i = currency.insert()
-	i.execute(name='Белорусский рубль', currency_code_num_3='974', currency_code_ascii_3='BYR', currency_exponent=0)
-	i.execute(name='Доллар США', currency_code_num_3='840', currency_code_ascii_3='USD', currency_exponent=2)
-	i.execute(name='Российский рубль', currency_code_num_3='643', currency_code_ascii_3='RUB', currency_exponent=2)
-	i.execute(name='Евро', currency_code_num_3='978', currency_code_ascii_3='EUR', currency_exponent=2)
+	i.execute(name='Белорусский рубль', code_num_3='974', code_ascii_3='BYR', exponent=0)
+	i.execute(name='Доллар США', code_num_3='840', code_ascii_3='USD', exponent=2)
+	i.execute(name='Российский рубль', code_num_3='643', code_ascii_3='RUB', exponent=2)
+	i.execute(name='Евро', code_num_3='978', code_ascii_3='EUR', exponent=2)
 
 	i = period_units.insert()
 	i.execute(name='день')
@@ -298,7 +298,7 @@ if current_metadata_revision == 1:
 	metadata_timestamp(metadata_comment)
 
 
-################################# OBJECTS MAPPING, SESSIONS AND RELATIONS
+################################# OBJECTS MAPPING AND RELATIONS
 
 print("\n")
 
@@ -338,65 +338,192 @@ user_mapper = mapper(User, users,
 			properties={'contacts': relation(contact_mapper)}
 		)
 
+def num_w_exp (value, expanant):
+	value / (10**exponent)
+
+def num_str_exp (value, expanent):
+	("%."+("%d" % exponent)+"f") % num_w_exp(value, expanent)
+
 class Currency(object):
-	def __init__(self, name=None, currency_code_num_3=None, currency_code_ascii_3=None, currency_exponent=0):
+	def __init__(self, name=None, code_num_3=None, code_ascii_3=None, exponent=0):
 		self.name = name
-		self.currency_code_num_3 = currency_code_num_3
-		self.currency_code_ascii_3 = currency_code_ascii_3
-		self.currency_exponent = currency_exponent
+		self.code_num_3 = code_num_3
+		self.code_ascii_3 = code_ascii_3
+		self.exponent = exponent
 	def __repr__(self):
-		return self.currency_code_ascii_3
+		return self.code_ascii_3
 	def f_val(value):
-		return value / (10**self.currency_exponent)
+		return num_w_exp(value, self.expanent)
 	def s_val(value):
-		return ("%."+("%d" % self.currency_exponent)+"f") % self.f_val(value) + " " + self.currency_code_ascii_3
+		return num_str_exp(value, self.expanent) + " " + self.code_ascii_3
  
 class PeriodUnit(object):
 	def __init__(self, name=None):
 		self.name = name
 	def __repr__(self):
 		return self.name
-	
+
 class UserPaymentObligation(object):
 	def __init__(self, user, currency=None, amount=0, period_unit=None, duration=0, start_date=None, end_date=None):
 		self.user = user
-		self.currency = currency if currency else session.query(Currency).filter_by(currency_code_ascii_3='BYR').first()
+		self.currency = currency if currency else session.query(Currency).filter_by(code_ascii_3='BYR').first()
 		self.amount = amount
 		self.period_unit = period_unit if period_unit else session.query(PeriodUnit).filter_by(name='месяц').first()
 		self.duration = duration
 		self.start_date = start_date if start_from else datetime.now
 		self.end_date = end_date
 	def __repr__(self):
-		return self.currency.s_val(self.amount) + " / " + self.duration + " " + self.period_unit + " from " + self.start_date + 
-			((" till " + self.end_date) if self.end_date else "")
-
-class RegularFixedPaymentPeriod(object):
-	def __init__(self, name=None):
-		self.name = name
-	def __repr__(self):
-		return self.name
+		return self.currency.s_val(self.amount) + " / " + self.duration + " " + self.period_unit + " from " + self.start_date + ((" till " + self.end_date) if self.end_date else "")
 
 class Payment(object):
-	def __init__(self, name=None):
-		self.name = name
+	def __init__(self, title=None):
+		self.title = title
 	def __repr__(self):
-		return self.name
+		return self.title
+
+class PaymentPeriod(object):
+	def __init__(self, payment, currency=None, amount=0, period_unit=None, duration=0, start_date=None, end_date=None):
+		self.payment = payment
+		self.currency = currency if currency else session.query(Currency).filter_by(code_ascii_3='BYR').first()
+		self.amount = amount
+		self.period_unit = period_unit if period_unit else session.query(PeriodUnit).filter_by(name='месяц').first()
+		self.duration = duration
+		self.start_date = start_date if start_from else datetime.now
+		self.end_date = end_date
+	def __repr__(self):
+		if self.duration != 0:
+			return self.currency.s_val(self.amount) + " / " + self.duration + " " + self.period_unit + " from " + self.start_date + ((" till " + self.end_date) if self.end_date else "")
+		else:
+			return self.currency.s_val(self.amount) + " till " + self.end_date
+
+class ExchangeEventData(object):
+	def __init__(self, src_currency, dst_currency, course, exponent, location=None):
+		self.src_currency = src_currency
+		self.dst_currency = dst_currency
+		self.course = course
+		self.exponent = exponent
+		self.location = location
+	def __repr__(self):
+		return self.src_currency + "/" + self.dst_currency + " " + num_str_exp(self.course, self.expanent)
 
 class Event(object):
-	def __init__(self, name=None):
-		self.name = name
+	def type_str (type_num):
+		return {
+			0 : "complex",
+			1 : "income",
+			2 : "payment",
+			3 : "slice",
+			4 : "merge",
+			5 : "exchange"
+			}[type_num];
+
+	def type_id (type_str):
+		return {
+			"complex" : 0,
+			"income" : 1,
+			"payment" : 2,
+			"slice" : 3,
+			"merge" : 4,
+			"exchange" : 5
+			}[type_str];
+
+	def type_name (self):
+		return type_str(self.type)
+
+	def __init__(self, type, event_timestamp=None, parent_event=None, src_user=None, dst_payment=None, exchange_event_data=None):
+		self.type = type
+		self.event_timestamp = event_timestamp
+		self.parent_event = parent_event
+		self.src_user = src_user
+		self.dst_payment = dst_payment
+		self.exchange_event_data = exchange_event_data
 	def __repr__(self):
-		return self.name
+		return self.type_name() + " " + self.event_timestamp + {
+			0 : "",
+			1 : " " + self.src_user,
+			2 : " " + self.dst_payment,
+			3 : "",
+			4 : "",
+			5 : " " + self.exchange_event_data
+			} [self.type]
 
 class MoneyChunk(object):
-	def __init__(self, name=None):
-		self.name = name
+	def __init__(self, currency, amount, src_event):
+		self.currency = currency
+		self.amount = amount
+		self.src_event = src_event
+		self.dst_event = None
 	def __repr__(self):
 		return self.name
 
 
+period_unit_mapper = mapper(PeriodUnit, period_units)
 
-#TODO: отдельные файлы: подключение к базе и настройки, создание и модификация схемы, metadata, тесты схемы, классы, тесты классов, функции-запросы, тесты функций, сервер, тесты сервера
+user_payment_obligation_mapper = mapper(UserPaymentObligation, users_payment_obligations,
+			properties={
+				'user' : relation(User),
+				'currency' : relation(Currency),
+				'period_unit' : relation(PeriodUnit)
+			}
+		)
+
+payment_period_mapper = mapper(PaymentPeriod, payment_periods,
+			properties={
+				'payment' : relation(Payment),
+				'currency' : relation(Currency),
+				'period_unit' : relation(PeriodUnit)
+			}
+		)
+
+exchange_event_data_mapper = mapper(ExchangeEventData, exchange_event_data,
+			properties={
+				'src_currency' : relation(Currency),
+				'dst_currency' : relation(Currency)
+			}
+		)
+
+money_chunk_mapper = mapper(MoneyChunk, money_chunks,
+			properties={
+				'currency' : relation(Currency),
+				'src_event' : relation(Event),
+				'dst_event' : relation(Event),
+			}
+		)
+
+event_mapper = mapper(Event, events,
+			properties={
+				'parent_event' : relation(Event),
+				'src_user' : relation(User),
+				'dst_payment' : relation(Payment),
+				'exchange_event_data' : relation(ExchangeEventData),
+
+				'child_events' : relation(event_mapper),
+				'money_chunks_src' : relation(money_chunk_mapper.src_event),
+				'money_chunks_dst' : relation(money_chunk_mapper.dst_event)
+			}
+		)
+
+
+payment_mapper = mapper(Payment, payments,
+			properties={
+				'periods' : relation(payment_period_mapper),
+				'events' : relation(event_mapper)
+			}
+		)
+
+currency_mapper = mapper(Currency, currency,
+			properties={
+				'money_chunks' : relation(money_chunk_mapper),
+				'user_payment_obligations' : relation(user_payment_obligation_mapper),
+				'exchange_event_data_src' : relation(exchange_event_data_mapper.src_currency),
+				'exchange_event_data_dst' : relation(exchange_event_data_mapper.dst_currency),
+				'payment_periods' : relation(payment_period_mapper)
+			}
+		)
+
+
+
+#TODO: операции для чисел с экспонентой
 
 ################################# TESTING
 
