c423a212bc269e45865110f8e9cc6a8cac67300b
Alex Gutikov <gutikoff@gmail.com>
2015-01-21 18:26:23 +0300

    transactions simple view with template

--- hckrspace/finance/urls.py
+++ hckrspace/finance/urls.py
@@ -4,4 +4,5 @@ from finance import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^transactions/$', views.TransactionList.as_view(), name='transactions'),
 ]
--- hckrspace/finance/views.py
+++ hckrspace/finance/views.py
@@ -1,5 +1,11 @@
 from django.http import HttpResponse
 
+from django.views.generic import ListView
+
+from finance.models import Subject, Currency, Contract, Bill, Transaction, Event, Payment, Membership_fee, Total, Balance
+
+class TransactionList(ListView):
+	model = Transaction
 
 def index(request):
     return HttpResponse("Hello, world. You're at the finance index.")
